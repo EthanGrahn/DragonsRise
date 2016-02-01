e8b70c65122e5411ccd83769bd53cd6d9d19133d
EthanGrahn <e_q101@live.com>
2016-02-01 15:55:41 -0600

    First File Setup

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneSwitch : MonoBehaviour {
+
+    /*=======================================================
+      Universal Scene Switcher
+      Attach to empty game object
+      Enter scene name to set scene that will be switched to
+      =======================================================*/  
+
+    public void SwitchScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Debug.Log("Scene changed to " + sceneName);
+    }
+}
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats : MonoBehaviour {
+
+    public int max_health;
+    public int current_health;
+    public int base_damage;
+    public int element_fire;
+    public int element_water;
+    public int element_earth;
+    public int level;
+
+    public void damage( int amount )
+    {
+        current_health -= amount;
+    }
+
+    public void heal( int amount )
+    {
+        current_health += amount;
+    }
+
+    public void max_healthChange( int amount )
+    {
+        max_health += amount;
+    }
+
+    public void base_damageChange( int amount )
+    {
+        base_damage += amount;
+    }
+
+    public void elementalDamageChange( int fire, int water, int earth )
+    {
+        element_fire += fire;
+        element_water += water;
+        element_earth += earth;
+    }
+
+    public void levelChange( int amount )
+    {
+        level += amount;
+    }
+}
