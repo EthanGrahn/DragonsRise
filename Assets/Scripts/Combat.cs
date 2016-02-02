8ca06d6a0d2a76d5df2a23ff28fceba3c4febdb9
Ethan Grahn <e_q101@live.com>
2016-02-02 00:44:54 -0600

    desktop

---
+++ Assets/Scripts/Combat.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Combat : MonoBehaviour {
+
+    public GameObject self;
+    public GameObject enemy;
+    private Stats enemyStats;
+    private Stats selfStats;
+    private bool enemyDead = false;
+    private bool selfDead = false;
+    private bool maxHealth = false;
+    private Text enemyHit;
+    private Text playerHealth;
+    private Text enemyHealth;
+
+    void Start()
+    {
+        enemyStats = enemy.GetComponent<Stats>();
+        selfStats = self.GetComponent<Stats>();
+
+        enemyHit = GameObject.Find("enemyHitTxt").GetComponent<Text>();
+        playerHealth = GameObject.Find("playerHealthTxt").GetComponent<Text>();
+        enemyHealth = GameObject.Find("enemyHealthTxt").GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (selfStats.current_health == selfStats.max_health)
+            maxHealth = true;
+        if (enemyStats.current_health <= 0)
+            enemyDead = true;
+        if (selfStats.current_health <= 0)
+            selfDead = true;
+
+        playerHealth.text = "Health: " + selfStats.current_health;
+        enemyHealth.text = "Health: " + enemyStats.current_health;
+    }
+
+    IEnumerator enemyDamage(float time)
+    {
+        enemyHit.text = "-20";
+        yield return new WaitForSeconds(2);
+        enemyHit.text = "";
+    }
+
+    public void Attack()
+    {
+        if (!enemyDead)
+        {
+            enemyStats.damage(20);
+            int health = enemyStats.current_health;
+            Debug.Log("Enemy health = " + health);
+            enemyHit.color = Color.red;
+            enemyDamage(2);
+        } 
+        else
+            Debug.Log("Enemy dead");
+    }
+
+    public void Heal()
+    {
+        if (!selfDead && !maxHealth)
+        {
+            selfStats.heal(10);
+            int health = selfStats.current_health;
+            Debug.Log("Character health = " + health);
+        } else if (maxHealth)
+            Debug.Log("You are at max health");
+        else
+            Debug.Log("You are dead");
+    }
+
+    public void Hit()
+    {
+        if (!selfDead)
+        {
+            selfStats.damage(5);
+        }
+    }
+}
---
+++ Assets/Scripts/MapGeneration.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGeneration : MonoBehaviour {
+
+    // Height and width of map grid
+    // Will possibly be a static value later
+    public int height;
+    public int width;
+
+	// Begins Generation
+	void Start ()
+    {
+        bool[,] map = new bool[height, width];
+    }
+}
