d3593f0ca7febf15a8ee80a7a68fade0d9a442f5
EthanGrahn <e_q101@live.com>
2016-02-19 00:26:29 -0600

    Final Test Commit
    Making sure .gitignore is working

--- Assets/Resources/Scripts/MapGeneration.cs
+++ Assets/Resources/Scripts/MapGeneration.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class MapGeneration : MonoBehaviour {
 
     // 5 x 5 map
-    //testing changes
+    //testing changes again
 
     private GameObject[] pieces = new GameObject[36];
 
