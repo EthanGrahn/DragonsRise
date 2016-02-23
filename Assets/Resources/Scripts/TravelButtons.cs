94509bb7a3e81a330ed62338d88cf12c889c4523
EthanGrahn <e_q101@live.com>
2016-02-23 10:34:38 -0600

    Map Traversal Buttons
    Completed the map traversal buttons (pretty sure), now I need to work on
    highlighting the currently active map piece for example purposes.

--- Assets/Resources/Scripts/TravelButtons.cs
+++ Assets/Resources/Scripts/TravelButtons.cs
@@ -5,34 +5,71 @@ public class TravelButtons : MonoBehaviour {
 
     private int current_index;
     private GameObject[] pieces;
+    private Mapping current_piece;
 
     void Start()
     {
         pieces = GameObject.Find("MapGenerator").GetComponent<MapGeneration>().pieces;
+        current_index = GameObject.Find("MapGenerator").GetComponent<MapGeneration>().current_index;
+        current_piece = pieces [current_index - 1].GetComponent<Mapping>();
     }
 
-    void Update()
-    {
-        current_index = GameObject.Find("MapGenerator").GetComponent<MapGeneration>().current_index;
-    }
+    // Shouldn't need the update function, but I can add it back in if needed
 
 	public void Left()
     {
-        
+        if (current_piece.left != null)
+        {
+            current_piece = current_piece.left.GetComponent<Mapping>();
+            current_index = current_piece.index;
+            Adjust();
+        } else
+        {
+            Debug.Log("You are on the edge of the map.");
+        }
     }
 
     public void Right()
     {
-
+        if (current_piece.right != null)
+        {
+            current_piece = current_piece.right.GetComponent<Mapping>();
+            current_index = current_piece.index;
+            Adjust();
+        } else
+        {
+            Debug.Log("You are on the edge of the map.");
+        }
     }
 
     public void Up()
     {
-
+        if (current_piece.up != null)
+        {
+            current_piece = current_piece.up.GetComponent<Mapping>();
+            current_index = current_piece.index;
+            Adjust();
+        } else
+        {
+            Debug.Log("You are on the edge of the map.");
+        }
     }
 
     public void Down()
     {
+        if (current_piece.down != null)
+        {
+            current_piece = current_piece.down.GetComponent<Mapping>();
+            current_index = current_piece.index;
+            Adjust();
+        } else
+        {
+            Debug.Log("You are on the edge of the map.");
+        }
+    }
 
+    private void Adjust()
+    {   // Changes the current in the MapGenerator object so it can do its work
+        GameObject.Find("MapGenerator").GetComponent<MapGeneration>().current_index = current_index;
     }
 }
