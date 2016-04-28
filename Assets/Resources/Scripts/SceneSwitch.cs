2d536867ba541b302e235b308baff8c25c1cc95e
Ethan Grahn <e_q101@live.com>
2016-04-28 02:16:14 -0500

    Main Menu Changes
    Button adjustments made requiring a new script and materials

--- Assets/Resources/Scripts/SceneSwitch.cs
+++ Assets/Resources/Scripts/SceneSwitch.cs
@@ -8,12 +8,7 @@ public class SceneSwitch : MonoBehaviour {
       Universal Scene Switcher
       Attach to empty game object
       Enter scene name to set scene that will be switched to
-      =======================================================*/  
-
-    void Update()
-    {
-
-    }
+      =======================================================*/
 
     public void SwitchScene(string sceneName)
     {
